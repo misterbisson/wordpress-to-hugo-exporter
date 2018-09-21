96c9e09fe6cc2e2e6078d11b471f3875b9170c4b
Casey Bisson <casey.bisson@gmail.com>
2018-09-20 19:40:20 -0700

    initial changes to support my sites

--- hugo-export.php
+++ hugo-export.php
@@ -36,7 +36,8 @@ class Hugo_Export
      *
      * @var bool
      */
-    private $include_comments = false; //export comments as part of the posts they're associated with
+    // set TRUE by Casey
+    private $include_comments = TRUE; //export comments as part of the posts they're associated with
 
     public $rename_options = array('site', 'blog'); //strings to strip from option keys on export
 
@@ -87,7 +88,6 @@ class Hugo_Export
      */
     function register_menu()
     {
-
         add_management_page(__('Export to Hugo', 'hugo-export'), __('Export to Hugo', 'hugo-export'), 'manage_options', 'export.php?type=hugo');
     }
 
@@ -98,6 +98,7 @@ class Hugo_Export
     function get_posts()
     {
 
+// @TODO: need to handle post_type=scrib-authority
         global $wpdb;
         return $wpdb->get_col("SELECT ID FROM $wpdb->posts WHERE post_status in ('publish', 'draft', 'private') AND post_type IN ('post', 'page' )");
     }
@@ -116,12 +117,24 @@ class Hugo_Export
     }
 
     /**
+     * @param WP_Post $post
+     *
+     * @return bool|string
+     */
+    protected function _getPostDateAsMemento(WP_Post $post)
+    {
+        $unixTime = strtotime($post->post_date_gmt);
+        return date('YmdHis', $unixTime);
+    }
+
+    /**
      * Convert a posts meta data (both post_meta and the fields in wp_posts) to key value pairs for export
      */
     function convert_meta(WP_Post $post)
     {
         $output = array(
             'title' => html_entity_decode(get_the_title($post), ENT_QUOTES | ENT_XML1, 'UTF-8'),
+            'slug' => $post->post_name,
             'author' => get_userdata($post->post_author)->display_name,
             'type' => get_post_type($post),
             'date' => $this->_getPostDateAsIso($post),
@@ -141,19 +154,44 @@ class Hugo_Export
             $output['private'] = true;
         }
 
-        //turns permalink into 'url' format, since Hugo supports redirection on per-post basis
-        if ('page' !== $post->post_type) {
-            $output['url'] = urldecode(str_replace(home_url(), '', get_permalink($post)));
+        // add URL aliases for the content
+        // https://gohugo.io/content-management/urls/
+        $output['aliases'] = array(
+            urldecode( str_replace( home_url(), '', get_permalink( $post ))),
+            '/?p=' . $post->ID,
+        );
+        if ('page' == $post->post_type)
+        {
+            $output['aliases'] = '/?page_id=' . $post->ID;
         }
+        $output['aliases'] = array_unique( $output['aliases'] );
+
+        $output['wp_guid'] = urldecode( $post->guid );
+
+
+        //turns permalink into 'url' format, since Hugo supports redirection on per-post basis
+//        $output['url'] = urldecode(str_replace(home_url(), '', get_permalink($post)));
 
         // check if the post or page has a Featured Image assigned to it.
         if (has_post_thumbnail($post)) {
-            $output['featured_image'] = str_replace(get_site_url(), "", get_the_post_thumbnail_url($post));
+            //$output['featured_image'] = str_replace(get_site_url(), "", get_the_post_thumbnail_url($post));
+            // added by Casey to get the original item
+            $output['featured_image'] = str_replace(get_site_url(), "", wp_get_attachment_url( get_post_thumbnail_id( $post->ID )));
         }
 
         //convert traditional post_meta values, hide hidden values
         foreach (get_post_custom($post->ID) as $key => $value) {
             if (substr($key, 0, 1) == '_') {
+                continue;
+            }
+            // exclusions added by Casey
+            if ( in_array( $key, array(
+                'mct_proposed_tags',
+                'go_oc_settings',
+                'bgeo',
+                'go-opencalais',
+                'yourls_shorturl',
+            ))) {
                 continue;
             }
             if (false === $this->_isEmpty($value)) {
@@ -216,11 +254,187 @@ class Hugo_Export
     }
 
     /**
+     * Find all URLs in the post
+     */
+    function find_urls( $post )
+    {
+        $content = apply_filters('the_content', $post->post_content);
+
+        preg_match_all( '#\bhttps?://[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/))#', $content, $urls );
+
+        if ( ! isset( $urls[0] ) )
+        {
+            return array();
+        }
+
+        sort( $urls[0] );
+        $urls = array_unique( $urls[0] );
+
+        return $urls;
+    }
+
+    /**
+     * the goal here is, primarily, to copy media
+     * used used in this post to the Hugo post bundle
+     *
+     * Local media:
+     *   - try to copy it on the file system to the content bundle
+     *   - else, try to find it in Internet Archive and download it
+     *
+     * Remote media:
+     *   - try to import it from the remote location into the content bundle
+     *   - else, try to find it in Internet Archive and download it
+     *
+     */
+    function convert_linked_media( $post , $dirpath )
+    {
+
+        global $wp_filesystem;
+        $source_dir = wp_upload_dir();
+        $source_dir = dirname( $source_dir['basedir'] );
+
+        $media_extensions = array(
+            'jpg',
+            'jpeg',
+            'gif',
+            'png',
+            'pdf',
+
+            'mp3',
+            'm4a',
+            'oga',
+
+            'mov',
+        );
+
+        $imported_media = $recovered_media = $lost_media = array();
+        foreach( $this->find_urls( $post ) as $k => $url )
+        {
+
+            // WordPress encodes ampersands; they need decoding
+            $url_stripped_amps = str_replace( '&amp;', '&', $url );
+
+            // ignore links that don't appear to be media
+            if ( ! in_array(
+                pathinfo( $url_stripped_amps, PATHINFO_EXTENSION ),
+                $media_extensions
+            ))
+            {
+                continue;
+            }
+
+            // try to process local links on the filesystem
+            if ( FALSE !== strpos( $url_stripped_amps, get_site_url( NULL, '', parse_url( $url_stripped_amps,  PHP_URL_SCHEME ))))
+            {
+                // get the relative URL
+                $url_stripped_amps = str_replace( get_site_url( NULL, '', parse_url ( $url_stripped_amps,  PHP_URL_SCHEME )), '', $url_stripped_amps );
+
+                // copy the file
+                $filename = pathinfo( $url_stripped_amps, PATHINFO_BASENAME );
+                $wp_filesystem->copy( $source_dir . $url_stripped_amps, $dirpath . $filename );
+
+                // record meta, then move on to the next URL (no further processing here)
+                $imported_media[ $filename ] = $url;
+                continue;
+            }
+
+            // If we are still here, that means it's a remote file
+            // construct a filename that includes the remote host name
+            $filename = sanitize_title_with_dashes( parse_url( $url_stripped_amps,  PHP_URL_HOST )) . '-' . pathinfo( $url_stripped_amps, PATHINFO_BASENAME );
+
+            // try to fetch remote object from the web
+            $get = wp_remote_post( $url_stripped_amps );
+            if ( is_array( $get ) && 200 == $get['response']['code'] )
+            {
+                // write the returned body to the file
+                $wp_filesystem->put_contents( $dirpath . $filename, $get['body'] );
+
+                // record meta, then move on to the next URL (no further processing here)
+                $imported_media[ $filename ] = $url;
+                continue;
+            }
+
+            // If we are still here, that means we got an error when attempting to download the remote file
+            // try to find it in the Internet Archive
+            // api docs at https://archive.org/help/wayback_api.php
+            $get = wp_remote_get( 'http://archive.org/wayback/available?timestamp=' . $this->_getPostDateAsMemento( $post ) . '&url=' . $url );
+            if ( is_array( $get ) && 200 == $get['response']['code'] )
+            {
+                $archive_deets = json_decode( $get['body'] );
+
+                if ( isset( $archive_deets->archived_snapshots->closest->url ))
+                {
+                    $archive_url =  str_replace( '/http', 'if_/http', $archive_deets->archived_snapshots->closest->url );
+fwrite(STDOUT, print_r( $archive_url, TRUE ) . "\n\n" );
+
+                    $get = wp_remote_post( $archive_url );
+                    if ( is_array( $get ) && 200 == $get['response']['code'] )
+                    {
+                        // write the returned body to the file
+                        $wp_filesystem->put_contents( $dirpath . $filename, $get['body'] );
+
+                        // record meta, then move on to the next URL (no further processing here)
+                        $recovered_media[ $filename ] = $url;
+                        continue;
+                    }
+                }
+            }
+
+            // If we are still here, that means we got an error when attempting to download the remote file FROM THE INTERNET ARCHIVE
+            // report the loss
+            // People should also try http://timetravel.mementoweb.org for more potential sources
+            $lost_media[ $filename ] = $url;
+        }
+
+        return array(
+            'imported-media' => $imported_media,
+            'recovered-media' => $recovered_media,
+            'lost-media' => $lost_media,
+        );
+    }
+
+    /**
+     * XXX
+     */
+    function convert_attachments( $postID , $dirpath )
+    {
+
+        global $wp_filesystem;
+
+        $children = get_children( array(
+        	'post_parent' => $postID,
+        	'post_type'   => 'attachment',
+        	'numberposts' => -1,
+        	'post_status' => 'any'
+        ));
+
+        $imported_attachments = array();
+        foreach ( $children as $child )
+        {
+            $source_url = wp_get_attachment_url( $child->ID );
+            $source = get_attached_file( $child->ID );
+            $filename = pathinfo( $source, PATHINFO_BASENAME );
+
+            $wp_filesystem->copy( $source, $dirpath . $filename );
+
+            $imported_attachments[ $filename ] = $source_url;
+        }
+
+        return array( 'imported-attachments' => $imported_attachments );
+    }
+
+    /**
      * Convert the main post content to Markdown.
      */
     function convert_content($post)
     {
+
         $content = apply_filters('the_content', $post->post_content);
+
+        // additional filters
+        // remove bSuite/bCMS innerindex blocks
+        $content = preg_replace( '/<div class="contents innerindex">.*?<\/div>/is', '', $content );
+
         $converter = new Markdownify\ConverterExtra;
         $markdown = $converter->parseString($content);
 
@@ -235,27 +449,48 @@ class Hugo_Export
     /**
      * Loop through and convert all comments for the specified post
      */
-    function convert_comments($post)
+    function convert_comments($post, $dirpath)
     {
         $args = array(
             'post_id' => $post->ID,
             'order' => 'ASC',   // oldest comments first
-            'type' => 'comment' // we don't want pingbacks etc.
+            'type' => 'comment'
         );
         $comments = get_comments($args);
         if (empty($comments)) {
             return '';
         }
 
+        $meta = array(
+            'title' => 'Comments on ' . html_entity_decode(get_the_title($post), ENT_QUOTES | ENT_XML1, 'UTF-8'),
+            'date' => $this->_getPostDateAsIso($post),
+        );
+
         $converter = new Markdownify\ConverterExtra;
-        $output = "\n\n## Comments";
-        foreach ($comments as $comment) {
-            $content = apply_filters('comment_text', $comment->comment_content);
-            $output .= "\n\n### Comment by " . $comment->comment_author . " on " . get_comment_date("Y-m-d H:i:s O", $comment) . "\n";
-            $output .= $converter->parseString($content);
+        $comment_text = "\n\n## Comments";
+        foreach ($comments as $comment)
+        {
+            $meta['comments'][ $comment->comment_ID ] = array(
+                'type' => $comment->comment_type,
+                'author' => $comment->comment_author,
+                'email' => $comment->comment_author_email,
+                'url' => esc_url( $comment->comment_author_url ),
+                'date_gmt' => date('c', strtotime( $comment->comment_date_gmt )),
+            );
+            $content = $meta['comments'][ $comment->comment_ID ]['text'] = $converter->parseString( get_comment_text( $comment->comment_ID ));
+            $comment_text .= "\n\n\n### Comment by " . $comment->comment_author . " on " . get_comment_date("Y-m-d H:i:s O", $comment) . "\n\n";
+            $comment_text .= $content;
         }
 
-        return $output;
+        // Hugo doesn't like word-wrapped permalinks
+        $output = Spyc::YAMLDump($meta, false, 0);
+        $output .= "\n---\n" . $comment_text;
+
+        // write out the comments as a separate file
+        $this->write($output, $dirpath . 'comments.md');
+
+        // return empty because we're writing the comments out as a separate file
+        return '';
     }
 
     /**
@@ -263,12 +498,27 @@ class Hugo_Export
      */
     function convert_posts()
     {
+        // remove WP's responsive image injection
+        // it makes the markup noisier and we want just the basics
+        // https://rudrastyh.com/wordpress/responsive-images.html
+        remove_filter( 'the_content', 'wp_make_content_images_responsive' );
+
         global $post;
 
         foreach ($this->get_posts() as $postID) {
             $post = get_post($postID);
             setup_postdata($post);
-            $meta = array_merge($this->convert_meta($post), $this->convert_terms($postID));
+
+            $dirpath = $this->make_dest_dir($post);
+
+            // get all post meta, terms, and other bits as a single array
+            $meta = array_merge(
+                $this->convert_meta($post),
+                $this->convert_terms($postID),
+                $this->convert_attachments($postID, $dirpath),
+                $this->convert_linked_media($post, $dirpath)
+            );
+
             // remove falsy values, which just add clutter
             foreach ($meta as $key => $value) {
                 if (!is_numeric($value) && !$value) {
@@ -282,9 +532,12 @@ class Hugo_Export
             $output .= "\n---\n";
             $output .= $this->convert_content($post);
             if ($this->include_comments) {
-                $output .= $this->convert_comments($post);
+                $output .= $this->convert_comments($post, $dirpath);
             }
-            $this->write($output, $post);
+            $this->write($output, $dirpath . 'index.md');
+
+            // write progress to the CLI
+            fwrite(STDOUT, '.' );
         }
     }
 
@@ -323,7 +576,9 @@ class Hugo_Export
 
         WP_Filesystem();
 
-        $this->dir = $this->getTempDir() . 'wp-hugo-' . md5(time()) . '/';
+        $urlbits = parse_url( site_url() );
+
+        $this->dir = $this->getTempDir() . 'wp-hugo-' . $urlbits['host'] . sanitize_title_with_dashes( $urlbits['path'] ) . '-' . md5(time()) . '/';
         $this->zip = $this->getTempDir() . 'wp-hugo.zip';
         $wp_filesystem->mkdir($this->dir);
         $wp_filesystem->mkdir($this->dir . $this->post_folder);
@@ -331,10 +586,11 @@ class Hugo_Export
 
         $this->convert_options();
         $this->convert_posts();
-        $this->convert_uploads();
-        $this->zip();
-        $this->send();
-        $this->cleanup();
+// Disabled by Casey
+//        $this->convert_uploads();
+//        $this->zip();
+//        $this->send();
+//        $this->cleanup();
     }
 
     /**
@@ -381,19 +637,31 @@ class Hugo_Export
     /**
      * Write file to temp dir
      */
-    function write($output, $post)
+    function make_dest_dir($post)
     {
 
         global $wp_filesystem;
 
         if (get_post_type($post) == 'page') {
-            $wp_filesystem->mkdir(urldecode($this->dir . $post->post_name));
-            $filename = urldecode($post->post_name . '/index.md');
+            $dirpath = urldecode($this->dir . $post->post_name . '/');
         } else {
-            $filename = $this->post_folder . date('Y-m-d', strtotime($post->post_date)) . '-' . urldecode($post->post_name) . '.md';
+            $dirpath = $this->dir . $this->post_folder . date('Y-m-d', strtotime($post->post_date)) . '-' . urldecode($post->post_name . '/');
         }
 
-        $wp_filesystem->put_contents($this->dir . $filename, $output);
+        $wp_filesystem->mkdir( $dirpath );
+        return $dirpath;
+    }
+
+    /**
+     * Write file to temp dir
+     */
+    function write($output, $writepath)
+    {
+
+//        fwrite(STDOUT, $writepath . "\n" );
+
+        global $wp_filesystem;
+        $wp_filesystem->put_contents($writepath, $output);
     }
 
     /**
@@ -568,6 +836,7 @@ class Hugo_Export
     }
 }
 
+global $je;
 $je = new Hugo_Export();
 
 if (defined('WP_CLI') && WP_CLI) {
